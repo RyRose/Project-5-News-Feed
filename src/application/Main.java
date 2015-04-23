22a80b5bab91403b2587e52ddf405692edbad877
StranskyPA <stranskypa@hendrix.edu>
2015-04-23 15:18:45 -0500

    Temp GUI
    Went ahead and built a temporary GUI. Idea is to use a TreeTableView in
    order to make the article cell expandable via a listener.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -10,10 +11,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("GUI Version 1.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			Scene scene = new Scene(root,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			//Setting window title
+			primaryStage.setTitle("RSS Reader");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -22,6 +27,6 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
-		new Thread( () -> main(null) );
+		//new Thread( () -> main(null) );
 	}
 }
