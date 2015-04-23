d7ca4a6bab6b321e01d82d0c7c01eaba6b7d3392
StranskyPA <stranskypa@hendrix.edu>
2015-04-23 16:45:03 -0500

    Some More Updates

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
+import javafx.scene.input.KeyCode;
 
 public class Controller {
 	//FXML Objects Not Involved in TreeTableView
@@ -18,6 +19,7 @@ public class Controller {
 	//TreeTableView Table and Columns
 	@FXML
 	private TreeTableView<Article> table;
+	//Not sure if these declarations are correct. If not, I'll work on it when I find the fix.
 	@FXML
 	private TreeTableColumn<Article, String> title = new TreeTableColumn<>("title");
 	@FXML
@@ -29,6 +31,7 @@ public class Controller {
 	
 	//Non FXML items
 	private ObservableList<Article> articles;
+	private String feedURL;
 	
 	@FXML
 	public void initialize() {
@@ -36,24 +39,22 @@ public class Controller {
 		table.setPlaceholder(new Label("Enter the RSS feed of your chosing above in order to view the related articles."));
 		table.getColumns().addAll(title, author, date, text);
 		table.setVisible(true);	
+		
+		userInput.setOnKeyPressed(event -> {
+			KeyCode key = event.getCode();
+			if (key == KeyCode.ENTER) {add();}
+		});
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@FXML
+	public void add() {
+		if (userInput.getText().length() == 0 || userInput.getText() == null) {
+			userInput.setPromptText("Please enter a URL before pressing the button or enter key.");
+			return;
+		}
+		feedURL = userInput.getText();
+		userInput.clear();
+	}
 	
 	@FXML
 	public void endApplication(){
